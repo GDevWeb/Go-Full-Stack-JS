3b5201fae74cf3bfd90eb7af6efc1e8ac2954a27
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-05-20 22:16:19 +0200

    chapitre 3 - Mettez en place un système d'authentification sur votre application - terminé

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Thing = require("./models/thing");
+
+const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/users"); 
+
 const app = express();
 
 // Connexion à MongoDB
@@ -32,54 +35,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Route POST pour ajouter un objet
-app.post("/api/stuff", (req, res, next) => {
-  console.log("Corps de la requête:", req.body);
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body,
-  });
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
-    .catch((error) => {
-      console.error("Erreur lors de l'enregistrement:", error);
-      res.status(400).json({ error });
-    });
-});
-
-// Route PUT pour modifier un objet
-app.put("/api/stuff/:id", (req, res, next) => {
-  console.log("ID:", req.params.id);
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-// Route GET pour récupérer les détails d'un objet
-app.get("/api/stuff/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
-    .catch((error) => res.status(404).json({ message: "Objet non trouvé !" }));
-});
-
-// Route GET pour récupérer la liste des objets
-app.get("/api/stuff", (req, res, next) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// Route DELETE pour supprimer un objet
-app.delete("/api/stuff/:id", (req, res, next) => {
-  console.log("ID:", req.params.id);
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) =>
-      res
-        .status(400)
-        .json({ message: "Erreur lors de la suppression de l'objet !" })
-    );
-});
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
